e04008eb930588d1f4e33fe327ce49112ba543dd
Kosuke Furuta <kosuke.furuta@owtell.com>
2025-05-30 18:04:45 +0900

    dev: update homepage text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white text-gray-900">
-      <h1 className="text-4xl font-bold">Welcome to Glowlits</h1>
+      <h1 className="text-4xl font-bold">Welcome to the dev version of Glowlits</h1>
     </main>
   )
 }
